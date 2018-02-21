fcad80d1f0636237476a25796e13d6ec4442c692
TranNgocKhoa <30297271+TranNgocKhoa@users.noreply.github.com>
2018-02-21 16:58:42 +0700

    RecyclerView Smooth Scroll in Scroll View

--- app/src/main/java/android/com/hotandcool/FoodDetailActivity.java
+++ app/src/main/java/android/com/hotandcool/FoodDetailActivity.java
@@ -1,5 +1,6 @@
 package android.com.hotandcool;
 
+import android.com.hotandcool.pseudodata.ItemInListData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -8,12 +9,16 @@ import android.support.v7.widget.RecyclerView;
 
 public class FoodDetailActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewMoreFood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
 
-
+        recyclerViewMoreFood = (RecyclerView) findViewById(R.id.recyclerViewMoreFood);
+        RecyclerView.LayoutManager verticalLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerViewMoreFood.setLayoutManager(verticalLayoutManager);
+        recyclerViewMoreFood.setAdapter(ItemInListData.getItems1(this, R.layout.item_in_list));
 
     }
 }
